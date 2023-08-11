37894eabb9f9433fe6ab7560ab58ee0f1df940b0
iam-Akashgiri <047a.giri@gmail.com>
2023-08-11 20:33:20 +0530

    first commit

---
+++ vmsapp/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+
+VEHICLE_CHOICES = (
+
+    ('Two Wheeler','2 Wheeler'),
+    ('Three Wheeler', '3 Wheeler'),
+    ('Four Wheeler','4 Wheeler')
+   
+)
+
+
+class Vehicle(models.Model):
+    vh_number = models.CharField(max_length=20,verbose_name="Vehicle Number",blank=False)
+    vh_type = models.CharField(max_length=20,verbose_name="Vehicle Type", choices=VEHICLE_CHOICES,blank=False)
+    vh_model = models.CharField(max_length=20,verbose_name="Vehicle Model",blank=False)
+    vh_disc = models.TextField(verbose_name="Vehicle Discription",blank=False)
+
+    class Meta:
+        db_table = "Vehicles"
